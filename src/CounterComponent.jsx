a9118455c8d3ba0850dc923dd20e58a387745862
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-10-26 04:13:25 +0330

    Making reducer,action and store

---
+++ src/CounterComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CounterComponent = () => {
+    return (
+        <div>
+            <h1>Number:</h1>
+            <button>Increase</button>
+        </div>
+    );
+};
+
+export default CounterComponent;
